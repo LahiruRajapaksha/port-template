ae92cf79fbce50e3478e898cb9d00fbecf4d291c
lahirurajapaksha <ltwrajapaksha@gmail.com>
2023-12-09 03:10:31 +0100

    update with readonly func

--- src/components/RecursiveTree/RecursiveTree.tsx
+++ src/components/RecursiveTree/RecursiveTree.tsx
@@ -33,69 +33,78 @@ const RecursiveComponent = (props: RecursiveComponentProps) => {
     handleUpdateNode,
     setselectedNodeId,
   } = props;
-
-  return (
-    <Box pl="20px" className="parentWrapper">
-      {data.map((item) => (
-        <Box key={item.id} className="parent">
-          <Box display="flex" width={1} mb={1} alignItems="center">
-            <Box width="50%">
-              <CustomTextField
-                onClick={() => setselectedNodeId(item.id)}
-                onChange={handleUpdateNode}
-                disableUnderline
-                defaultValue={item.name}
-              />
-            </Box>
-
-            {selectedNodeId === item.id && (
-              <Box display="flex" ml="auto" alignItems="center">
-                <Box
-                  display="flex"
-                  alignItems="center"
-                  bgcolor="#e0e0e0"
-                  p={1}
-                  borderRadius={1}
-                  height="30px"
-                >
-                  <Box display="flex" alignItems="center">
-                    <Typography>Read Only</Typography>
-                    <Switch />
-                  </Box>
-                  <IconButton
-                    aria-label="delete"
-                    size="small"
-                    onClick={handleDeleteButtonClick}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </Box>
-                <AddRemoveButton
-                  variant="outlined"
-                  onClick={handleAddButtonClick}
-                  padding={2}
-                  margin="3px"
-                >
-                  <AddIcon />
-                </AddRemoveButton>
+    const [checked, setChecked] = React.useState(false);
+    const handleReadOnlyChange = (
+      event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+      setChecked(event.target.checked);
+    };
+    return (
+      <Box pl="20px" className="parentWrapper">
+        {data.map((item) => (
+          <Box key={item.id} className="parent">
+            <Box display="flex" width={1} mb={1} alignItems="center">
+              <Box width="50%">
+                <CustomTextField
+                  onClick={() => setselectedNodeId(item.id)}
+                  onChange={handleUpdateNode}
+                  disableUnderline
+                  defaultValue={item.name}
+                  readOnly={checked && selectedNodeId === item.id}
+                />
               </Box>
+
+              {selectedNodeId === item.id && (
+                <Box display="flex" ml="auto" alignItems="center">
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    bgcolor="#e0e0e0"
+                    p={1}
+                    borderRadius={1}
+                    height="30px"
+                  >
+                    <Box display="flex" alignItems="center">
+                      <Typography>Read Only</Typography>
+                      <Switch
+                        checked={checked}
+                        onChange={handleReadOnlyChange}
+                      />
+                    </Box>
+                    <IconButton
+                      aria-label="delete"
+                      size="small"
+                      onClick={handleDeleteButtonClick}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </Box>
+                  <AddRemoveButton
+                    variant="outlined"
+                    onClick={handleAddButtonClick}
+                    padding={2}
+                    margin="3px"
+                  >
+                    <AddIcon />
+                  </AddRemoveButton>
+                </Box>
+              )}
+            </Box>
+            {item.children && (
+              <RecursiveComponent
+                data={item.children}
+                level={level + 1}
+                handleAddButtonClick={handleAddButtonClick}
+                handleDeleteButtonClick={handleDeleteButtonClick}
+                setselectedNodeId={setselectedNodeId}
+                selectedNodeId={selectedNodeId}
+                handleUpdateNode={handleUpdateNode}
+              />
             )}
           </Box>
-          {item.children && (
-            <RecursiveComponent
-              data={item.children}
-              level={level + 1}
-              handleAddButtonClick={handleAddButtonClick}
-              handleDeleteButtonClick={handleDeleteButtonClick}
-              setselectedNodeId={setselectedNodeId}
-              selectedNodeId={selectedNodeId}
-              handleUpdateNode={handleUpdateNode}
-            />
-          )}
-        </Box>
-      ))}
-    </Box>
-  );
+        ))}
+      </Box>
+    );
 };
 
 export default RecursiveComponent;
